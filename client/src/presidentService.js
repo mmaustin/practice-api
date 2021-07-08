455674534edd9456641aefffbb8a5e5a8b2d010f
McCray <tubach75@hotmail.com>
2021-07-08 14:15:56 -0400

    presidentService class

--- client/src/presidentService.js
+++ client/src/presidentService.js
@@ -1,3 +1,8 @@
+const div = document.querySelector("#presidents-container")
+//const divTwo = document.querySelector("#achievements-container")
+const innerDiv = document.createElement('div')
+const d = document.createElement('div')
+
 class PresidentService {
     constructor(endpoint){
         this.endpoint = endpoint
@@ -6,7 +11,22 @@ class PresidentService {
     getPresidents(){
         fetch(`${this.endpoint}/presidents`)
         .then(resp => resp.json())
-        .then(presidents)
+        .then(presidents => {
+            for(const prez of presidents){
+                
+                div.innerHTML += `<div id="${prez.id}">${prez.name}</div>`
+                //innerDiv.appendChild(p)
+                //div.appendChild(d)
+
+                /*prez.achievements.forEach(a => {
+                    const pTwo = document.createElement('p')
+                    pTwo.innerText = `${a.title}`
+                    innerDiv.appendChild(pTwo)
+                    //console.log(a)
+                    div.appendChild(innerDiv)
+                })*/
+            }
+        })
     }
 
 }
